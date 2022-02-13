87f78263da01d60b0bce9ad0f57463467a511fd1
catcarbonell <catcarbonell@gmail.com>
2022-02-12 17:20:23 -0800

    fixes: database.js: ssl stuff

--- config/database.js
+++ config/database.js
@@ -15,10 +15,13 @@ const parse = require('pg-connection-string').parse;
             port: config.port,
             database: config.database,
             username: config.user,
-            password: config.password
+            password: config.password,
+            ssl: {
+              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+            },  
           },
           options: {
-            ssl: false
+            ssl: env.bool('DATABASE_SSL', false),
           }
         }
       }
