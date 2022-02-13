b794802696f8380e1adaa2b26b4117ef7474ec77
catcarbonell <catcarbonell@gmail.com>
2022-02-13 13:01:39 -0800

    adds: port in server.ks

--- env/production/server.js
+++ env/production/server.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   url: env('MY_HEROKU_URL'),
+  port: process.env.PORT,
   app: {
     keys: env.array("APP_KEYS", ["Tax+iYqIPK31eLXMpIWtQw==", "IboCMf6x00tPcQARDpZ7Dg==", "dNy8PT4nYfbM5bEzUAvcvg==", "Gdnk+WA49iOkKP7/DfaoBw=="]),
   },
