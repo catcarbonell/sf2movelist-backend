56b22e67da30786bb2a69624e737e8fdd9a4040a
catcarbonell <catcarbonell@gmail.com>
2022-02-14 09:15:47 -0800

    adds: keys to the production and config files

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', '1337'),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['Tax+iYqIPK31eLXMpIWtQw==', 'IboCMf6x00tPcQARDpZ7Dg==']),
   },
 });
--- env/production/server.js
+++ env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   url: env('MY_HEROKU_URL'),
   port: process.env.PORT,
   app: {
-    keys: env.array("APP_KEYS", ['testKey1', 'testKey2']),
+    keys: env.array('APP_KEYS', ['testKey1', 'testKey2']),
   },
 });
