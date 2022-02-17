c9f16b237ce928bb11a9e7e45b966cc2590a3401
catcarbonell <catcarbonell@gmail.com>
2022-02-16 16:22:57 -0800

    fixes: changed where env/production folder is

--- config/database.js
+++ config/database.js
@@ -1,13 +1,11 @@
 const path = require('path');
 const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
  
  module.exports = ({ env }) => {
   if(env('NODE_ENV') === 'production') {
     const config = parse(process.env.DATABASE_URL)
     return {
-      defaultConnection: 'default',
-      connections: {
+      connection: {
         default: {
           connector: 'bookshelf',
           settings: {
